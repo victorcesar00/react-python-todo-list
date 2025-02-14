4387faa9f70a1d1590aa5b415ec44c2e17015914
victor.lightbase <victor.lightbase@sebraemg.com.br>
2025-02-14 03:20:24 -0300

    Configures absolute imports, ES2022, and connection to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKEND_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
