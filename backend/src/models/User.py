eea4866c6338028d06216efee7c1ac94a211ddd9
victor.lightbase <victor.lightbase@sebraemg.com.br>
2025-02-14 18:01:18 -0300

    Switches legacy List to list

--- backend/src/models/User.py
+++ backend/src/models/User.py
@@ -1,4 +1,3 @@
-from typing import List
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
 
@@ -9,4 +8,4 @@ class User(Base):
     username: Mapped[str] = mapped_column(unique=True, index=True)
     password: Mapped[str] = mapped_column()
 
-    todos: Mapped[List['Todo']] = relationship(back_populates='owner')
+    todos: Mapped[list['Todo']] = relationship(back_populates='owner', lazy='noload')
