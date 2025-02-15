c90d0c0ec419682360273c1e56ff57e5e334d42d
victor.lightbase <victor.lightbase@sebraemg.com.br>
2025-02-14 21:43:57 -0300

    Removes list of todos from userResponse interface

--- frontend/src/http/dtos/response/IUserResponseDTO.ts
+++ frontend/src/http/dtos/response/IUserResponseDTO.ts
@@ -1,7 +1,4 @@
-import ITodoResponseDTO from '@/http/dtos/response/ITodoResponseDTO'
-
 export default interface IUserResponseDTO {
     id: number
     username: string
-    todos: ITodoResponseDTO[]
 }
