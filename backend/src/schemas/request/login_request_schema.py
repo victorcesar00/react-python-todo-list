b7e36596dee6dafa0aed865eb3adaafb5f46622b
victor.lightbase <victor.lightbase@sebraemg.com.br>
2025-02-15 04:59:22 -0300

    Implements validations on login request

--- backend/src/schemas/request/login_request_schema.py
+++ backend/src/schemas/request/login_request_schema.py
@@ -1,5 +1,9 @@
+from fastapi import FastAPI, Query
+from typing import Annotated
 from pydantic import BaseModel
 
+app = FastAPI()
+
 class LoginRequestSchema(BaseModel):
-    username: str
-    password: str
+    username: Annotated[str, Query(min_length=5, regex="^[a-zA-Z0-9_]*$")]
+    password: Annotated[str, Query(min_length=5)]
